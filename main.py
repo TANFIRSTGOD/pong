f07235781b4497950d29cf2fa1a2827a999477dd
tan05 <tan.tantham.tan@gmail.com>
2023-11-18 10:06:48 +0700

    playerMovement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 
+plr1y = 300
+plr2y = 300
+
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("PONG")
@@ -12,8 +15,20 @@ while running:
             running = False
 
     screen.fill("black")
-    plr1 = pygame.draw.rect(screen, (255,255,255), rect=(100,300,20,150))
-    plr2 = pygame.draw.rect(screen, (255,255,255), rect=(1180,300,20,150))
+    plr1 = pygame.draw.rect(screen, (255,255,255), rect=(100,plr1y,20,150))
+    plr2 = pygame.draw.rect(screen, (255,255,255), rect=(1180,plr2y,20,150))
+    ball = pygame.draw.circle(screen, (255,255,255), radius=10, center=(640,360))
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        plr1y -= 5
+    elif keys[pygame.K_s]:
+        plr1y += 5
+    elif keys[pygame.K_UP]:
+        plr2y -= 5
+    elif keys[pygame.K_DOWN]:
+        plr2y += 5
+
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
