3918534b5d77c4e55d3f0c559f87df60df9cde60
tan05 <tan.tantham.tan@gmail.com>
2023-11-18 09:54:02 +0700

    draw players

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("PONG")
 clock = pygame.time.Clock()
 running = True
 
@@ -11,7 +12,8 @@ while running:
             running = False
 
     screen.fill("black")
-    pygame.draw.rect(screen, (255,255,255), rect=(0,0,50,100))
+    plr1 = pygame.draw.rect(screen, (255,255,255), rect=(100,300,20,150))
+    plr2 = pygame.draw.rect(screen, (255,255,255), rect=(1180,300,20,150))
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
